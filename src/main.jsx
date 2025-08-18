df9c388865d739ce70bfdea6a67c6b6fd4d01a0b
jupitters <sampaiobale@gmail.com>
2025-08-18 17:26:11 -0300

    componets on main

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Content from './Content.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <App />
+    <Content />
+    <Footer />
   </StrictMode>,
 )
