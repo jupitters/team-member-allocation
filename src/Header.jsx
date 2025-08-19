fe7223813f6e9e03a0c043e319d92e2c9f5b84fe
jupitters <sampaiobale@gmail.com>
2025-08-19 17:12:35 -0300

    created employees

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-        <h1>Header</h1>
+        <h1 className='mt-4 p5 bg-primary text-white rounded'>Header</h1>
     </header>
   )
 }
