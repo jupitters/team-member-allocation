478f499f197c135d54ae082ad871338fe2543b7d
jupitters <sampaiobale@gmail.com>
2025-08-21 01:13:28 -0300

    dropdown teams

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -5,6 +5,8 @@ import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
 
+    const [selectedTeam, setSelectedTeam] = useState("TeamB");
+
     const [employees, setEmployees] = useState([{
     id: 1,
     fullName: "Bob Jones",
@@ -92,9 +94,19 @@ const Employees = () => {
 
   return (
     <main className='container'>
-      <div class='row justify-content-center mt-3 mb-3'>
-        <div class='col-8'>
-          <div class='card-collection'>
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-6'>
+          <select className='form-select form-select-lg' value={selectedTeam}>
+            <option value='TeamA'>Team A</option>
+            <option value='TeamB'>Team B</option>
+            <option value='TeamC'>Team C</option>
+            <option value='TeamD'>Team D</option>
+          </select>
+        </div>
+      </div>
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-8'>
+          <div className='card-collection'>
             {
               employees.map((employee) => (
                 <div id={employee.id} className='card m-2' style={{cursor: "pointer"}}>
