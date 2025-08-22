78fd7feece1b4d3065859e2d92d61bf4b3c3596d
jupitters <sampaiobale@gmail.com>
2025-08-22 15:17:52 -0300

    navbar component

---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expanded-lg navbar-light bg-light">
+
+    </nav>
+  )
+}
+
+export default Nav
