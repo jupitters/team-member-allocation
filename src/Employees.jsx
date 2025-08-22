593dee9190122367e6477228699088a199889941
jupitters <sampaiobale@gmail.com>
2025-08-22 01:03:11 -0300

    key prop on employees

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -20,7 +20,7 @@ const Employees = ({employees, selectedTeam, handlerSelectTeams, handlerEmployee
           <div className='card-collection'>
             {
               employees.map((employee) => (
-                <div id={employee.id} className={(employee.teamName === selectedTeam?'card m-2 standout':'card m-2')} style={{cursor: "pointer"}} onClick={handlerEmployeeCardClick}>
+                <div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam?'card m-2 standout':'card m-2')} style={{cursor: "pointer"}} onClick={handlerEmployeeCardClick}>
                   {(employee.gender === 'female')? <img src={femaleProfile} className='card-img-top'/>:
                                                 <img src={maleProfile} className='card-img-top' />}
                   <div className='card-body'>
