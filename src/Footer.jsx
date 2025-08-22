c376757fb1dd8024b1cab80fbd046c43d2686cf0
jupitters <sampaiobale@gmail.com>
2025-08-22 00:56:06 -0300

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Footer = () => {
+  let today = new Date();git 
+
   return (
-    <footer>
-        <p>This is a footer!</p>
+    <footer className='container'>
+      <div className='row justify-content-center mt-3 mb-4'>
+        <div className='col-8'>
+          <h5>Team Member Allocation - {today}</h5>
+        </div>
+      </div>
     </footer>
   )
 }
