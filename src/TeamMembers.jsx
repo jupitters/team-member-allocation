2ab76ec428580dc628c7a8c21a0c4b6d64b6b7c7
jupitters <sampaiobale@gmail.com>
2025-08-28 17:11:00 -0300

    team member component

--- src/TeamMembers.jsx
+++ src/TeamMembers.jsx
@@ -5,14 +5,7 @@ import maleProfile from './images/maleProfile.jpg';
 const TeamMembers = ({employees, selectedTeam, handlerEmployeeCardClick}) => {
   return (
     employees.map((employee) => (
-                    <div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam?'card m-2 standout':'card m-2')} style={{cursor: "pointer"}} onClick={handlerEmployeeCardClick}>
-                      {(employee.gender === 'female')? <img src={femaleProfile} className='card-img-top'/>:
-                                                    <img src={maleProfile} className='card-img-top' />}
-                      <div className='card-body'>
-                        <h5 className='card-title'>Full Name: {employee.fullName}</h5>
-                        <p className='card-text'><b>Designation:</b> {employee.designation}</p>
-                      </div>
-                    </div>
+                    <TeamMemberCard handlerEmployeeCardClick={handlerEmployeeCardClick} selectedTeam={selectedTeam} employee={employee}/>
                   ))
   )
 }
